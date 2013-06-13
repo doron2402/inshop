a8b7cbea77d21101806a19e79e8a9391fd2ab993
Doron Segal <doronsegal@Dorons-MacBook-Pro.local>
2013-06-13 17:22:58 +0300

    Adding twitter bootstrap

--- protected/config/main.php
+++ protected/config/main.php
@@ -10,7 +10,7 @@ return array(
 	'name'=>'My Web Application',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	'preload'=>array('log','bootstrap'),
 
 	// autoloading model and component classes
 	'import'=>array(
@@ -20,14 +20,16 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'admin',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
+			'generatorPaths'=>array(
+            	'bootstrap.gii',
+        	),
 		),
-		*/
 	),
 
 	// application components
@@ -36,6 +38,11 @@ return array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
+		
+		'bootstrap'=>array(
+        	'class'=>'ext.bootstrap.components.bootstrap',
+    	),
+    	
 		// uncomment the following to enable URLs in path-format
 		/*
 		'urlManager'=>array(
