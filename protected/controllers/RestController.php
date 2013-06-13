4f2ea669999f26ed832bccb89017904fba70a186
Doron Segal <doronsegal@Dorons-MacBook-Pro.local>
2013-06-13 16:15:02 +0300

    RestController

---
+++ protected/controllers/RestController.php
@@ -0,0 +1,34 @@
+<?php
+
+class RestClient extends Controller
+{
+
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+		// renders the view file 'protected/views/site/index.php'
+		// using the default layout 'protected/views/layouts/main.php'
+		$this->render('index');
+	}
+
+	/**
+	 * This is the action to handle external exceptions.
+	 */
+	public function actionError()
+	{
+		//redirect when there's an error or wrong request to the server
+	}
+
+	public function actionLogin()
+	{
+		# code...
+	}
+
+	public function actionLogout()
+	{
+		# code...
+	}
+}
